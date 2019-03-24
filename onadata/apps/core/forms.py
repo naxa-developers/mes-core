4f6e1cd8ea244e42d3631143a9aca0890ead6895
MaskeyPuja <MaskeyPuja@github.com>
2019-03-24 16:16:20 +0545

    form modified

--- onadata/apps/core/forms.py
+++ onadata/apps/core/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from django.forms import widgets
 
 from onadata.apps.logger.models import XForm
 from .models import Project, Output, ActivityGroup, Activity, Cluster, Beneficiary, UserRole
@@ -14,12 +16,30 @@ class SignUpForm(UserCreationForm):
 		fields = ('username', 'email', 'password',)
 
 
+# class DivWrapperWidget(widgets.TextInput):
+# 	# def render(self, id, max_length, name, placeholder, type):
+# 		return mark_safe(u'''<div class="form-group">%s</div>''' % (super(DivWrapperWidget, self).render(id, max_length, name, placeholder, type)))
+
 class ProjectForm(forms.ModelForm):
+	# FORM_CHOICES = (
+	# 		('yes', 'yes'),('no', 'no'),
+	# 	)
+
+
+	# beneficiaries = forms.ChoiceField(widget=forms.RadioSelect(),choices=FORM_CHOICES)
 
 	class Meta:
 		model = Project
+		
 		fields = ('name', 'description', 'sector', 'start_date', 'end_date', 'reporting_period', 'beneficiaries')
 		
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Name','class': 'form-control'}),
+			'description': forms.Textarea(attrs={'placeholder': 'Description','class': 'form-control'}),
+			'sector': forms.TextInput(attrs={'placeholder': 'Sector','class': 'form-control'}),
+			'start_date': forms.TextInput(attrs={'placeholder': 'Start date','class': 'form-control', 'type': 'date'}),
+			'end_date': forms.TextInput(attrs={'placeholder': 'End date','class': 'form-control', 'type': 'date'}),
+		}
 
 
 class OutputForm(forms.ModelForm):
@@ -28,13 +48,32 @@ class OutputForm(forms.ModelForm):
 		model = Output
 		fields = ('name', 'description','project')
 
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
+			'description': forms.Textarea(attrs={'placeholder': 'Description', 'class': 'form-control'}),
+		}
+
+		def __init__(self, *args, **kwargs):
+			super().__init__(*args, **kwargs)
+			self.fields['project'].queryset = Project.objects.none()
+
+
 
 class ActivityGroupForm(forms.ModelForm):
 
 	class Meta:
 		model = ActivityGroup
-		fields = ('output', 'project', 'name', 'description')
+		fields = ('name', 'description', 'output', 'project')
 
+		widgets = {
+			'name' : forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
+			'description' : forms.Textarea(attrs={'placeholder': 'Description', 'class': 'form-control'}),
+		}
+
+		# def __init__(self, *args, **kwargs):
+		# 	super().__init__(*args, **kwargs)
+		# 	self.fields['output'].queryset = Output.objects.none()
+		# 	self.fields['project'].queryset = Project.objects.none()			
 
 
 class ActivityForm(forms.ModelForm):
@@ -47,19 +86,48 @@ class ActivityForm(forms.ModelForm):
 		model = Activity
 		fields = ('activity_group', 'name', 'description', 'target_number', 'target_unit', 'start_date', 'end_date', 'form', 'beneficiary_level')
 
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Name','class': 'form-control'}),
+			'description': forms.Textarea(attrs={'placeholder': 'Description','class': 'form-control'}),
+			'target_number': forms.TextInput(attrs={'placeholder': 'Target Number','class': 'form-control'}),
+			'target_unit': forms.TextInput(attrs={'placeholder': 'Target Unit','class': 'form-control'}),
+			'start_date': forms.TextInput(attrs={'placeholder': 'Start date','class': 'form-control', 'type': 'date'}),
+			'end_date': forms.TextInput(attrs={'placeholder': 'End date','class': 'form-control', 'type': 'date'}),
+		}
+
 
 class ClusterForm(forms.ModelForm):
 
 	class Meta:
 		model = Cluster
-		fields = ('name', 'project', 'district', 'municipality', 'ward')
+		fields = ('name', 'district', 'municipality', 'ward', 'project')
+
+
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
+			'district': forms.TextInput(attrs={'placeholder': 'District', 'class': 'form-control'}),
+			'municipality': forms.TextInput(attrs={'placeholder': 'Municipality', 'class': 'form-control'}),
+			'ward': forms.TextInput(attrs={'placeholder': 'Ward', 'class': 'form-control'}),
+		}
+
+		def __init__(self, *args, **kwargs):
+			super().__init__(*args, **kwargs)
+			self.fields['project'].queryset = Project.objects.none()
 
 
 class BeneficiaryForm(forms.ModelForm):
 
 	class Meta:
 		model = Beneficiary
-		fields = ('name', 'address', 'ward_no', 'cluster', 'Type')
+		fields = ('name', 'address', 'ward_no', 'Type', 'cluster')
+
+
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
+			'address': forms.TextInput(attrs={'placeholder': 'Address', 'class': 'form-control'}),
+			'ward_no': forms.TextInput(attrs={'placeholder': 'Ward Number', 'class': 'form-control'}),
+			'Type': forms.TextInput(attrs={'placeholder': 'Type', 'class': 'form-control'}),
+		}
 
 
 class UserRoleForm(forms.ModelForm):
