3a73da0f1d39386fcc76949fd474fc70fbb7a4ef
SanipRijal <saneprijal@gmail.com>
2019-04-04 12:51:41 +0545

    merge migrations with master

---
+++ onadata/apps/core/migrations/0036_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0035_auto_20190404_1035'),
+        ('core', '0031_merge'),
+    ]
+
+    operations = [
+    ]
