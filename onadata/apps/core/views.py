3e04557aee94fed4e3e889a52cbd0ce120b9dff1
MaskeyPuja <MaskeyPuja@github.com>
2019-04-10 14:22:49 +0545

    404_error page rendered

--- onadata/apps/core/views.py
+++ onadata/apps/core/views.py
@@ -53,7 +53,32 @@ class HomeView(LoginRequiredMixin, TemplateView):
         elif self.request.group.name in ['project-manager', 'super-admin']:
             return render(request, self.template_name)
         else:
-            raise PermissionDenied()
+            return HttpResponseRedirect(reverse('404_error'))
+            # raise PermissionDenied()
+
+    # def get_context_data(request, **kwargs):
+    #     import ipdb
+    #     ipdb.set_trace()
+
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+
+    #     output = Output.objects.all()
+    #     output_count = Output.objects.all().count()
+    #     context['output'] = output
+    #     context['output_count'] = output_count
+    #     print(context)
+    #     return context
+
+
+class ProjectDashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'core/project-dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        output = Output.objects.all()
+        context['output'] = output
+        return context
 
 
 class SignInView(TemplateView):
@@ -129,6 +154,8 @@ class ProjectDeleteView(ManagerMixin, DeleteView):
 class OutputListView(ManagerMixin, ListView):
     model = Output
     template_name = 'core/output-list.html'
+
+
 
 
 class OutputDetailView(ManagerMixin, DetailView):
@@ -510,7 +537,8 @@ class UserActivityViewSet(viewsets.ModelViewSet):
         elif role.group.name == 'super-admin':
             queryset = ClusterA.objects.all()
         else:
-            raise PermissionDenied()
+            return HttpResponseRedirect(reverse('404_error'))
+            # raise PermissionDenied()
         return queryset
 
 
