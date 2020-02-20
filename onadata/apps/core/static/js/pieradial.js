51ddf3e81d45cdf3dbb0df2b315b3035f5aa4f74
sanip <saneprijal@gmail.com>
2020-02-20 14:34:30 +0545

    order of phases

--- onadata/apps/core/static/js/pieradial.js
+++ onadata/apps/core/static/js/pieradial.js
@@ -33,6 +33,9 @@ $(document).ready(function(){
         dataType: 'json',        
 
         success: function(result){
+            result = result.sort(function(a, b){
+                return parseFloat(a.percentage) - parseFloat(b.percentage);
+            })
             $("#radialloader").css("display", "none");  
             var myseries = [];
             var radius = ["110%", "80%", "50%"];
@@ -138,6 +141,9 @@ $(document).ready(function(){
                     data: {'district': request.id},            
             
                     success: function(result){
+                        result = result.sort(function(a, b){
+                            return parseFloat(a.percentage) - parseFloat(b.percentage);
+                        })
                         $("#district-"+request.id+"-loader").css("display", "none");
                         i = 0;
                         $("#district_"+ request.id +"_legend").empty();
