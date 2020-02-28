d16aea3b500cd1dcc42d8d19f8924c53fbf09f5e
sanip <saneprijal@gmail.com>
2020-02-28 13:23:23 +0545

    create new table

--- onadata/apps/core/utils.py
+++ onadata/apps/core/utils.py
@@ -656,18 +656,14 @@ def create_db_table(submission):
         data = get_question_answer(submission)
         args = get_arguments(data)
         table_name = submission.cluster_activity.cag.activity_group.name
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute('SAVEPOINT sp1')
-                command = "CREATE TABLE {} (id SERIAL PRIMARY KEY)".format(table_name)
+        with connection.cursor() as cursor:
+            command = "CREATE TABLE {} (id SERIAL PRIMARY KEY)".format(table_name)
+            cursor.execute(command)
+            for key, value in args.items():
+                command = "ALTER TABLE {0} ADD COLUMN {1} {2}".format(table_name, key, value)
                 cursor.execute(command)
-                for key, value in args.items():
-                    command = "ALTER TABLE {0} ADD COLUMN {1} {2}".format(table_name, key, value)
-                    cursor.execute(command)
-            return True
-        except:
-            cursor.execute('ROLLBACK TO SAVEPOINT sp1')
-            return "error"
+        return True
+
     else:
         return False
 
